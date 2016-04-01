75bb74da4223968b201fcd36b213772344b582b7
DongHeeYun <inter0960@naver.com>
2016-04-01 20:05:08 +0900

    메세지 형식으로 보여줌

--- CallHelper/app/src/main/java/supernova/com/callhelper/MainActivity.java
+++ CallHelper/app/src/main/java/supernova/com/callhelper/MainActivity.java
@@ -1,14 +1,17 @@
 package supernova.com.callhelper;
 
 import android.Manifest;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +20,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Button goBtn, updateBtn;
-    EditText filename, editText;
+    EditText filename;
+    LinearLayout line;
     private String[] per =  new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};    //sd카드에 저장하고 읽어오는 퍼미션
     String fileName;
     SpeechManager manager = new SpeechManager(this);
     ArrayList<String> results = new ArrayList<String>();
-    String totalText="";
+    int msgNum=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         goBtn = (Button)findViewById(R.id.goBtn);
         filename = (EditText)findViewById(R.id.filename);
-        editText = (EditText)findViewById(R.id.editText);
-        updateBtn = (Button)findViewById(R.id.updateBtn);
+        line = (LinearLayout)findViewById(R.id.line1);
 
         ActivityCompat.requestPermissions(this, per, 2);    //권한 설정 나중엔 권한 확인하고 없으면 권한설정으로 바꿔야함, 어플 실행시로 이동하면 좋을듯
 
@@ -40,32 +43,34 @@ public class MainActivity extends AppCompatActivity {
         goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                manager.addFile("rec1.pcm");
-                manager.addFile("rec2.pcm");
-                manager.addFile("rec3.pcm");
-                manager.addFile("rec4.pcm");
-
-            }
-        });
-        updateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(totalText);
-                Toast.makeText(MainActivity.this, totalText, Toast.LENGTH_LONG).show();
+                manager.addFile(filename.getText().toString());
             }
         });
     }
 
-    public void setTotalText(String totalText){
-        this.totalText = totalText;
+    public void addResult(String result){
+        results.add(result);
     }
 
     Handler handler = new Handler(){
         public void handleMessage(Message msg) {
-            Toast.makeText(MainActivity.this, totalText, Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, results.get(0), Toast.LENGTH_SHORT).show();
             if(msg.what==0){
-                editText.setText(totalText);
+                addMessage(results.get(msgNum));
+                msgNum++;
             }
         }
     };
+
+    public void addMessage(String msg){
+        TextView tv = new TextView(MainActivity.this);
+        tv.setText(msg);
+        tv.setBackgroundColor(Color.YELLOW);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(5,5,5,5);
+        tv.setTextSize(20);
+        tv.setLayoutParams(params);
+
+        line.addView(tv);
+    }
 }
