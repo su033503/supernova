ef0c390b1da5e59260d7d05fef20686f8c0f598c
DongHeeYun <inter0960@naver.com>
2016-04-01 18:47:06 +0900

    http통신으로 google speech api에 접속하고 결과를 받아와 EditText에 출력하는거 가능

--- CallHelper/app/src/main/java/supernova/com/callhelper/MainActivity.java
+++ CallHelper/app/src/main/java/supernova/com/callhelper/MainActivity.java
@@ -1,117 +1,71 @@
 package supernova.com.callhelper;
 
-import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Environment;
+import android.Manifest;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.Scanner;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2;
-    TextView tv;
-    Integer resCode = -1;
+    Button goBtn, updateBtn;
+    EditText filename, editText;
+    private String[] per =  new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};    //sd카드에 저장하고 읽어오는 퍼미션
+    String fileName;
+    SpeechManager manager = new SpeechManager(this);
+    ArrayList<String> results = new ArrayList<String>();
+    String totalText="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goBtn = (Button)findViewById(R.id.goBtn);
+        filename = (EditText)findViewById(R.id.filename);
+        editText = (EditText)findViewById(R.id.editText);
+        updateBtn = (Button)findViewById(R.id.updateBtn);
 
-        File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/good");
-        f.mkdir();
+        ActivityCompat.requestPermissions(this, per, 2);    //권한 설정 나중엔 권한 확인하고 없으면 권한설정으로 바꿔야함, 어플 실행시로 이동하면 좋을듯
 
-        btn1 = (Button)findViewById(R.id.btn1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        manager.start();
+
+        goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new HttpThreadClass().execute();
-                Toast.makeText(getApplicationContext(), resCode.toString(),Toast.LENGTH_SHORT).show();
+                manager.addFile("rec1.pcm");
+                manager.addFile("rec2.pcm");
+                manager.addFile("rec3.pcm");
+                manager.addFile("rec4.pcm");
+
             }
         });
-        tv = (TextView)findViewById(R.id.textView);
-        tv.setText("how");
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(totalText);
+                Toast.makeText(MainActivity.this, totalText, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    private class HttpThreadClass extends AsyncTask<String,String,String>{
+    public void setTotalText(String totalText){
+        this.totalText = totalText;
+    }
 
-        String speechApiURL = "https://www.google.com/speech-api/v2/recognize";
-        String apiKey = "AIzaSyAjMHB85JfptvetxeHbHdwcrY8HnWdtnKI";
-        String conURL = speechApiURL+"?output=json&lang=ko-KR&key="+apiKey;
-
-        @Override
-        protected String doInBackground(String... params) {
-            doitHttp();
-            return null;
-        }
-
-        private void doitHttp(){
-            byte[] data=null;
-            OutputStream out = null;
-
-            Scanner inSt = null;
-            String res="";
-            try {
-                URL url = new URL(conURL);
-                URLConnection con = url.openConnection();
-                HttpsURLConnection httpconn = (HttpsURLConnection)con;
-                httpconn.setAllowUserInteraction(false);
-                httpconn.setInstanceFollowRedirects(true);
-                httpconn.setRequestMethod("POST");
-                httpconn.setDoOutput(true);
-                httpconn.setChunkedStreamingMode(0);
-                httpconn.setRequestProperty("Content-Type", "audio/l16; rate=16000");
-
-                httpconn.connect();
-
-                try{
-                    String fileDir = Environment.getExternalStorageDirectory().getAbsolutePath()+"/rec1.pcm";
-                    File file = new File(fileDir);
-                    FileInputStream fis = new FileInputStream(file);
-                    FileChannel fc = fis.getChannel();
-
-                    int size = (int)fc.size();
-                    MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY,0,size);
-                    data = new byte[bb.remaining()];
-                    bb.get(data);
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-
-                try{
-                    out = httpconn.getOutputStream();
-                    out.write(data);
-                    resCode = httpconn.getResponseCode();
-
-
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                inSt = new Scanner(httpconn.getInputStream());
-                while(inSt.hasNext()){
-                    res+=(inSt.nextLine());
-                }
-                tv.setText(res);
-            }catch (Exception e){
-                e.printStackTrace();
+    Handler handler = new Handler(){
+        public void handleMessage(Message msg) {
+            Toast.makeText(MainActivity.this, totalText, Toast.LENGTH_LONG).show();
+            if(msg.what==0){
+                editText.setText(totalText);
             }
         }
-    }
+    };
 }
